223a471eeff00c4bc38381ec2952d7591b0280fc
jeffrey125 <jeffreyasilo8@gmail.com>
2022-05-02 14:28:15 +0800

    Added context on HeaderCardButton

--- src/Components/Layout/HeaderCardButton.js
+++ src/Components/Layout/HeaderCardButton.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
 
 const HeaderCardButton = props => {
+  const ctx = useContext(CartContext);
+
+  const numberOfCartItems = ctx.items.reduce(
+    (acc, curr) => acc + curr.amount,
+    0
+  );
+
   return (
     <button onClick={props.onShowModal} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
