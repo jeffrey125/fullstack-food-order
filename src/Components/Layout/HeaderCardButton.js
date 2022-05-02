3dfacdb8ded8b6cd8bdfdc8ede7ea3727c3046d4
jeffrey125 <jeffreyasilo8@gmail.com>
2022-05-02 22:15:18 +0800

    Added btn bump animation

--- src/Components/Layout/HeaderCardButton.js
+++ src/Components/Layout/HeaderCardButton.js
@@ -1,9 +1,10 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 import CartContext from '../../store/cart-context';
 
 const HeaderCardButton = props => {
+  const [btnBump, setBtnBump] = useState(false);
   const ctx = useContext(CartContext);
 
   const numberOfCartItems = ctx.items.reduce(
@@ -11,8 +12,23 @@ const HeaderCardButton = props => {
     0
   );
 
+  const btnClasses = `${classes.button} ${btnBump ? classes.bump : ''}`;
+
+  // Button bump animation
+  useEffect(() => {
+    if (ctx.items.length === 0) return;
+
+    setBtnBump(true);
+
+    const bumpAnimation = setTimeout(() => {
+      setBtnBump(false);
+    }, 200);
+
+    return () => clearTimeout(bumpAnimation);
+  }, [ctx.items, numberOfCartItems]);
+
   return (
-    <button onClick={props.onShowModal} className={classes.button}>
+    <button onClick={props.onShowModal} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
