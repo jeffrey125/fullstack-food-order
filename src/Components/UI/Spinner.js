6021424c6967685af213c34fe230ffcd284a4429
jeffrey125 <jeffreyasilo8@gmail.com>
2022-05-10 23:04:57 +0800

    Removed console.log in spinner

--- src/Components/UI/Spinner.js
+++ src/Components/UI/Spinner.js
@@ -2,7 +2,6 @@ import React from 'react';
 import classes from './Spinner.module.css';
 
 const Spinner = props => {
-  console.log(props);
   return (
     <p className={`${classes.paragraph} ${props.className}`}>
       {props.children}
